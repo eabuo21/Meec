0c455ec603e29aac5d89ef05605e702357b4e19f
eabuo21 <fabloz12g@gmail.com>
2024-06-25 08:14:20 -0700

    n

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,20 +1,12 @@
-/** @format */
-
 import React, { useEffect } from "react";
 import SHero from "../Components/Services/SHero";
-import SSubHero from "../Components/Services/SSubHero";
 
 const Services = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  return (
-    <div className="service-container w-full h-[fixed] flex flex-col  bg-dance_blue">
-      <SHero className="hero-section bg-white w-full h-[fixed] pb-8" />
-      <SSubHero className="hero-sub-section w-full h-[fixed]  "/>
-    </div>
-  );
+  return <div>Services</div>;
 };
 
 export default Services;
